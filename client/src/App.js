3f84bf1368ab4551a153e1250c72894736ced1a3
vanesa-tamu <vahe5495@tamu.edu>
2020-02-25 14:24:50 -0600

    routes rendering with move and movielist componenets

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
-
+import { Route } from 'react-router-dom'
 import SavedList from './Movies/SavedList';
+
+import Movie from './Movies/Movie.js'
+import MovieList from './Movies/MovieList.js'
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
@@ -13,6 +16,13 @@ const App = () => {
     <div>
       <SavedList list={savedList} />
       <div>Replace this Div with your Routes</div>
+
+      <Route exact path='/'>
+        <MovieList />
+      </Route>
+      <Route path='/movies/:movieID'>
+        <Movie/>
+      </Route>
     </div>
   );
 };
