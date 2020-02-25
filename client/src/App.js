831fd93c608c6f083519b91a5df6597a33a70eab
vanesa-tamu <vahe5495@tamu.edu>
2020-02-25 16:38:55 -0600

    can see movies individually

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
-import { Route } from 'react-router-dom'
+import { Route , Switch , Link} from 'react-router-dom'
 import SavedList from './Movies/SavedList';
 
 import Movie from './Movies/Movie.js'
 import MovieList from './Movies/MovieList.js'
 
 const App = () => {
-  const [savedList, setSavedList] = useState( [] );
+  const [savedList, setSavedList] = useState([]);
 
   const addToSavedList = movie => {
     setSavedList( [...savedList, movie] );
@@ -15,14 +15,18 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+
+      <Switch >
+      <Route path='/movies/:movieID'>
+        <Movie/>
+      </Route> 
 
       <Route exact path='/'>
         <MovieList />
       </Route>
-      <Route path='/movies/:movieID'>
-        <Movie/>
-      </Route>
+     
+
+      </Switch>
     </div>
   );
 };
