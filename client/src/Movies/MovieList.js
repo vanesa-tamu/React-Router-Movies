1497a0e4ac0730abc3aa3dfa985a9b487b71fd42
vanesa-tamu <vahe5495@tamu.edu>
2020-02-27 13:18:33 -0600

    small changes

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -30,7 +30,7 @@ const MovieList = props => {
 function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
   return (
-    <Link to={`/movies/${movie.id}`}> 
+    <Link to={`/movies/${movie.id}`} className='link-router'> 
     <div className="movie-card">
       <h2>{title}</h2>
       <div className="movie-director">
